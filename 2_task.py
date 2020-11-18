e5ba60f50f042781d66f8809a1028cafd305e3f2
Mokyton <72911121+Mokyton@users.noreply.github.com>
2020-11-18 20:26:16 +0300

    Update 2_task.py

--- 2_task.py
+++ 2_task.py
@@ -1,5 +1,6 @@
-summ = 0
+summ = []
 for i in range(1000, 20001):
-    if i % 3 == 0 or i % 5 == 0:
-        summ += 1
-print(summ)
+    if i % 3 == 0 and i % 5 == 0:
+       summ.append(i)
+
+print(sum(summ))
